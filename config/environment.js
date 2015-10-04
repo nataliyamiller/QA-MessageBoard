e2e5b5be0bdc5a526d73103a6b9267e64dcd4c5a
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-03 21:10:43 -0700

    Adds questions model

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'qa-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://question-and-answers.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
