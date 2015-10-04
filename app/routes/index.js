2aad1e332ced42cfaa0b1e7a624ca5f5a647f7b0
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-03 22:15:57 -0700

    Adds a 'question' route

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,15 +17,5 @@ export default Ember.Route.extend({
       question.destroyRecord();
       this.transitionTo('index');
     },
-
-    updateQuestion(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key] !== undefined) {
-          question.set(key, params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    },
   }
 });
