342cc0c44c80b2b456912661b5bf84fe93b684e6
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-03 21:56:26 -0700

    Adds update buttonfor questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,27 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
+    deleteQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+
+    updateQuestion(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+  }
 });
